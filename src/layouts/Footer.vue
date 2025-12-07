d07c73e432b5cb796b99a5ccb5dadd2881d02817
brobro332 <332bro@naver.com>
2025-12-07 14:33:20 +0900

    feat: 메인화면 개발

---
+++ src/layouts/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+	<footer class="footer">
+		<div class="left black-text">© 2025. ORCA STUDIO All rights reserved.</div>
+
+		<div class="right">
+			<a href="#">
+				<img src="@/assets/kakao.svg" alt="카톡 문의" class="kakao-btn" />
+			</a>
+		</div>
+	</footer>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.footer {
+	width: 100%;
+	height: 50px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 0 20px;
+	background-color: white;
+	box-sizing: border-box;
+}
+
+.left {
+	color: black;
+}
+
+.kakao-btn {
+	width: 30px;
+	height: 30px;
+	object-fit: contain;
+}
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<h1 class="black-text">ORCA STUDIO<sup>®</sup></h1>
+	</div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h1 {
+	color: #333;
+}
+</style>
